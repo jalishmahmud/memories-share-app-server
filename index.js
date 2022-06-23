a5a8b735a2982f7f52eb85bc9d74f93123e1d7fb
jalishmahmud <jalish93@gmail.com>
2022-06-23 18:37:03 +0600

    initial express server and mongoose setup done

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import mongoose from "mongoose"
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }))
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }))
+
+app.use(cors())
+
+const CONNECTION_URL = 'mongodb+srv://reactProject:React1234@cluster0.kte3f.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 4000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+// mongoose.set('useFindAndModify', false)
